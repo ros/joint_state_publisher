102c8729faa580196c2c30adcee5845e5a60d792
Chris Lalancette <clalancette@openrobotics.org>
2019-12-19 20:48:48 +0000

    Remove GUI parts from joint_state_publisher.
    This will be moved to a separate package.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

---
+++ joint_state_publisher/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['joint_state_publisher'],
+    package_dir={'': 'src'}
+)
+
+setup(**d)
