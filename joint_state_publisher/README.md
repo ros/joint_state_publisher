d5b5c05d51a8725a466acdc6e5eecc71b8c4c020
Chris Lalancette <clalancette@openrobotics.org>
2020-01-13 13:17:43 -0500

    Split jsp and jsp gui (#31)
    * Split jsp and jsp_gui.
    
    This allows us to have joint_state_publisher not depend on python_qt_bindings, which ensures that we don't drag that dependency into the robot metapackage.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

---
+++ joint_state_publisher/README.md
@@ -0,0 +1,34 @@
+# Joint State Publisher
+
+This contains a package for publishing `sensor_msgs/JointState` messages for a robot described with URDF.
+Given a URDF in the `robot_description` parameter on the parameter server, this node
+will continually publish default values for all of the movable joints in the URDF to the `/joint_states` topic.
+
+See the ROS wiki for additional API documentation and tutorials.
+
+* [`joint_state_publisher`](http://wiki.ros.org/joint_state_publisher)
+
+This was originally part of the [`ros/robot_model`](https://github.com/ros/robot_model) repository.
+It has been moved to this repo as described by [`ros/robot_model#195`](https://github.com/ros/robot_model/issues/195)
+
+Published Topics
+----------------
+* `/joint_states` (`sensor_msgs/JointState`) - The state of all of the movable joints in the system.
+
+Subscribed Topics
+-----------------
+* (optional) `/any_topic` (`sensor_msgs/JointState`) - If the `sources_list` parameter is not empty (see Parameters below), then every named topic in this parameter will be subscribed to for joint state updates.  Do *not* add the default `/joint_states` topic to this list, as it will end up in an endless loop!
+
+Parameters
+----------
+* `rate` (int) - The rate at which to publish updates to the `/joint_states` topic.  Defaults to 10.
+* `publish_default_positions` (bool) - Whether to publish a default position for each movable joint to the `/joint_states` topic.  If False, `use_gui` must also be False.  Defaults to True.
+* `publish_default_velocities` (bool) - Whether to publish a default velocity for each movable joint to the `/joint_states` topic.  Defaults to False.
+* `publish_default_efforts` (bool) - Whether to publish a default effort for each movable joint to the `/joint_states` topic.  Defaults to False.
+* `use_mimic_tags` (bool) - Whether to honor `<mimic>` tags in the URDF.  Defaults to True.
+* `use_smallest_joint_limits` (bool) - Whether to honor `<safety_controller>` tags in the URDF.  Defaults to True.
+* `source_list` (array of strings) - Each string in this array represents a topic name.  For each string, create a subscription to the named topic of type `sensor_msgs/JointStates`.  Publication to that topic will update the joints named in the message.  Defaults to an empty array.
+* `zeros` (dictionary of string -> float) - A dictionary of joint_names to initial starting values for the joint.  Defaults to an empty dictionary, in which case 0.0 is assumed as the zero for all joints.
+* `dependent_joints` (dictionary of string -> dictionary of 'parent', 'factor', 'offset') - A dictionary of joint_names to the joints that they mimic; compare to the `<mimic>` tag in URDF.  A joint listed here will mimic the movements of the 'parent' joint, subject to the 'factor' and 'offset' provided.  The 'parent' name must be provided, while the 'factor' and 'offset' parameters are optional (they default to 1.0 and 0.0, respectively).  Defaults to the empty dictionary, in which case only joints that are marked as `<mimic>` in the URDF are mimiced.
+* `use_gui` (bool) - Deprecated option to show a GUI useful for manipulating joints when launching the joint_state_publisher.  Defaults to False.  Will be removed in Noetic in favor of running `joint_state_publisher_gui` instead.
+* `num_rows` (int) - Deprecated option to control the number of rows shown in the GUI when `use_gui` is set to true.  Defaults to the number of joints.  Will be removed in Noetic.
---
+++ joint_state_publisher_gui/src/joint_state_publisher_gui/__init__.py
@@ -0,0 +1,228 @@
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2010, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Willow Garage, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import math
+import random
+
+import rospy
+
+from python_qt_binding.QtCore import pyqtSlot
+from python_qt_binding.QtCore import Qt
+from python_qt_binding.QtCore import Signal
+from python_qt_binding.QtGui import QFont
+from python_qt_binding.QtWidgets import QApplication
+from python_qt_binding.QtWidgets import QHBoxLayout
+from python_qt_binding.QtWidgets import QLabel
+from python_qt_binding.QtWidgets import QLineEdit
+from python_qt_binding.QtWidgets import QPushButton
+from python_qt_binding.QtWidgets import QSlider
+from python_qt_binding.QtWidgets import QVBoxLayout
+from python_qt_binding.QtWidgets import QGridLayout
+from python_qt_binding.QtWidgets import QScrollArea
+from python_qt_binding.QtWidgets import QSpinBox
+from python_qt_binding.QtWidgets import QWidget
+
+RANGE = 10000
+
+
+class JointStatePublisherGui(QWidget):
+    sliderUpdateTrigger = Signal()
+
+    def __init__(self, title, jsp, num_rows=0):
+        super(JointStatePublisherGui, self).__init__()
+        self.jsp = jsp
+        self.joint_map = {}
+        self.vlayout = QVBoxLayout(self)
+        self.scrollable = QWidget()
+        self.gridlayout = QGridLayout()
+        self.scroll = QScrollArea()
+        self.scroll.setWidgetResizable(True)
+
+        self.jsp.set_source_update_cb(self.source_update_cb)
+
+        font = QFont("Helvetica", 9, QFont.Bold)
+
+        ### Generate sliders ###
+        sliders = []
+        for name in self.jsp.joint_list:
+            if name not in self.jsp.free_joints:
+                continue
+            joint = self.jsp.free_joints[name]
+
+            if joint['min'] == joint['max']:
+                continue
+
+            joint_layout = QVBoxLayout()
+            row_layout = QHBoxLayout()
+
+            label = QLabel(name)
+            label.setFont(font)
+            row_layout.addWidget(label)
+            display = QLineEdit("0.00")
+            display.setAlignment(Qt.AlignRight)
+            display.setFont(font)
+            display.setReadOnly(True)
+            row_layout.addWidget(display)
+
+            joint_layout.addLayout(row_layout)
+
+            slider = QSlider(Qt.Horizontal)
+
+            slider.setFont(font)
+            slider.setRange(0, RANGE)
+            slider.setValue(RANGE/2)
+
+            joint_layout.addWidget(slider)
+
+            self.joint_map[name] = {'slidervalue': 0, 'display': display,
+                                    'slider': slider, 'joint': joint}
+            # Connect to the signal provided by QSignal
+            slider.valueChanged.connect(lambda event,name=name: self.onValueChangedOne(name))
+
+            sliders.append(joint_layout)
+
+        # Determine number of rows to be used in grid
+        self.num_rows = num_rows
+        # if desired num of rows wasn't set, default behaviour is a vertical layout
+        if self.num_rows == 0:
+            self.num_rows = len(sliders)  # equals VBoxLayout
+        # Generate positions in grid and place sliders there
+        self.positions = self.generate_grid_positions(len(sliders), self.num_rows)
+        for item, pos in zip(sliders, self.positions):
+            self.gridlayout.addLayout(item, *pos)
+
+        # Set zero positions read from parameters
+        self.center()
+
+        # Synchronize slider and displayed value
+        self.sliderUpdate(None)
+
+        # Set up a signal for updating the sliders based on external joint info
+        self.sliderUpdateTrigger.connect(self.updateSliders)
+
+        self.scrollable.setLayout(self.gridlayout)
+        self.scroll.setWidget(self.scrollable)
+        self.vlayout.addWidget(self.scroll)
+
+        # Buttons for randomizing and centering sliders and
+        # Spinbox for on-the-fly selecting number of rows
+        self.randbutton = QPushButton('Randomize', self)
+        self.randbutton.clicked.connect(self.randomize_event)
+        self.vlayout.addWidget(self.randbutton)
+        self.ctrbutton = QPushButton('Center', self)
+        self.ctrbutton.clicked.connect(self.center_event)
+        self.vlayout.addWidget(self.ctrbutton)
+        self.maxrowsupdown = QSpinBox()
+        self.maxrowsupdown.setMinimum(1)
+        self.maxrowsupdown.setMaximum(len(sliders))
+        self.maxrowsupdown.setValue(self.num_rows)
+        self.maxrowsupdown.valueChanged.connect(self.reorggrid_event)
+        self.vlayout.addWidget(self.maxrowsupdown)
+        self.setLayout(self.vlayout)
+
+    def source_update_cb(self):
+        self.sliderUpdateTrigger.emit()
+
+    def onValueChangedOne(self, name):
+        # A slider value was changed, but we need to change the joint_info metadata.
+        joint_info = self.joint_map[name]
+        joint_info['slidervalue'] = joint_info['slider'].value()
+        joint = joint_info['joint']
+        joint['position'] = self.sliderToValue(joint_info['slidervalue'], joint)
+        joint_info['display'].setText("%.2f" % joint['position'])
+
+    @pyqtSlot()
+    def updateSliders(self):
+        self.update_sliders()
+
+    def update_sliders(self):
+        for name, joint_info in self.joint_map.items():
+            joint = joint_info['joint']
+            joint_info['slidervalue'] = self.valueToSlider(joint['position'],
+                                                           joint)
+            joint_info['slider'].setValue(joint_info['slidervalue'])
+
+    def center_event(self, event):
+        self.center()
+
+    def center(self):
+        rospy.loginfo("Centering")
+        for name, joint_info in self.joint_map.items():
+            joint = joint_info['joint']
+            joint_info['slider'].setValue(self.valueToSlider(joint['zero'], joint))
+
+    def reorggrid_event(self, event):
+        self.reorganize_grid(event)
+
+    def reorganize_grid(self, number_of_rows):
+        self.num_rows = number_of_rows
+
+        # Remove items from layout (won't destroy them!)
+        items = []
+        for pos in self.positions:
+            item = self.gridlayout.itemAtPosition(*pos)
+            items.append(item)
+            self.gridlayout.removeItem(item)
+
+        # Generate new positions for sliders and place them in their new spots
+        self.positions = self.generate_grid_positions(len(items), self.num_rows)
+        for item, pos in zip(items, self.positions):
+            self.gridlayout.addLayout(item, *pos)
+
+    def generate_grid_positions(self, num_items, num_rows):
+        if num_rows == 0:
+            return []
+        positions = [(y, x) for x in range(int((math.ceil(float(num_items) / num_rows)))) for y in range(num_rows)]
+        positions = positions[:num_items]
+        return positions
+
+    def randomize_event(self, event):
+        self.randomize()
+
+    def randomize(self):
+        rospy.loginfo("Randomizing")
+        for name, joint_info in self.joint_map.items():
+            joint = joint_info['joint']
+            joint_info['slider'].setValue(
+                    self.valueToSlider(random.uniform(joint['min'], joint['max']), joint))
+
+    def sliderUpdate(self, event):
+        for name, joint_info in self.joint_map.items():
+            joint_info['slidervalue'] = joint_info['slider'].value()
+        self.update_sliders()
+
+    def valueToSlider(self, value, joint):
+        return (value - joint['min']) * float(RANGE) / (joint['max'] - joint['min'])
+
+    def sliderToValue(self, slider, joint):
+        pctvalue = slider / float(RANGE)
+        return joint['min'] + (joint['max']-joint['min']) * pctvalue
