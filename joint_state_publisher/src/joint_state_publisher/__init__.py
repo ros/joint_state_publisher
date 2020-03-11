b9d8a761321478fb946c6f45b3ace0259b669e9d
Shane Loretz <sloretz@openrobotics.org>
2020-03-11 18:45:16 -0400

    Set source_update_cb attr before creating subscribers (#41)
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- joint_state_publisher/src/joint_state_publisher/__init__.py
+++ joint_state_publisher/src/joint_state_publisher/__init__.py
@@ -160,15 +160,15 @@ class JointStatePublisher():
         else:
             self.init_urdf(robot)
 
-        source_list = get_param("source_list", [])
-        self.sources = []
-        for source in source_list:
-            self.sources.append(rospy.Subscriber(source, sensor_msgs.msg.JointState, self.source_cb))
-
         # The source_update_cb will be called at the end of self.source_cb.
         # The main purpose it to allow external observes (such as the
         # joint_state_publisher_gui) to be notified when things are updated.
         self.source_update_cb = None
+
+        source_list = get_param("source_list", [])
+        self.sources = []
+        for source in source_list:
+            self.sources.append(rospy.Subscriber(source, sensor_msgs.msg.JointState, self.source_cb))
 
         self.pub = rospy.Publisher('joint_states', sensor_msgs.msg.JointState, queue_size=5)
 
